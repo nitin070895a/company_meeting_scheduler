e8ff0578127d6996e798bba8f76edb4faff9c099
nitin070895a <nitin070895@gmail.com>
2018-09-01 20:17:41 +0530

    Remove focus from edittext on entering

--- app/src/main/java/com/example/companymeetingscheduler/Activity/ScheduleMeeting.java
+++ app/src/main/java/com/example/companymeetingscheduler/Activity/ScheduleMeeting.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.companymeetingscheduler.R;
@@ -13,14 +15,15 @@ import com.example.companymeetingscheduler.R;
  * Screen that lets the user to schedule a meeting
  * Created by Nitin on 01/09/18.
  */
-public class ScheduleMeeting extends AppCompatActivity {
+public class ScheduleMeeting extends AppCompatActivity implements View.OnClickListener {
 
     public static final String INTENT_DATA_DATE = "date";
 
     private EditText date;
     private EditText startTime;
     private EditText endTime;
-    private EditText desc;
+
+    private Button submit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,7 +34,11 @@ public class ScheduleMeeting extends AppCompatActivity {
         date = findViewById(R.id.date);
         startTime = findViewById(R.id.startTime);
         endTime = findViewById(R.id.endTime);
-        desc = findViewById(R.id.description);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(this);
+        startTime.setOnClickListener(this);
+        endTime.setOnClickListener(this);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -68,6 +75,29 @@ public class ScheduleMeeting extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.startTime:
+
+                break;
+
+            case R.id.endTime:
+
+                break;
+
+            case R.id.submit:
+
+                break;
+        }
+    }
+
+    /**
+     * Removes focus from edit text, but make it clickable
+     * @param editText the edit text to be made clickable but not editable
+     */
     private void makeEditTextImutable(EditText editText) {
 
         editText.setFocusable(false);
