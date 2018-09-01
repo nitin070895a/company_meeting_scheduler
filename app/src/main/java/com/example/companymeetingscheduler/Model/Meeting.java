4f23dbe7f291910506ba846ae296fa0ae20eeb08
nitin070895a <nitin070895@gmail.com>
2018-09-01 15:11:02 +0530

    Created model for meeting

---
+++ app/src/main/java/com/example/companymeetingscheduler/Model/Meeting.java
@@ -0,0 +1,68 @@
+package com.example.companymeetingscheduler.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Contains all the data representing a single meeting
+ * Created by Nitin on 01/09/18.
+ */
+public class Meeting {
+
+    /**
+     * The start time of the meeting in format
+     * {@link com.example.companymeetingscheduler.Helper.TimeAndDateUtils#DEFAULT_DATE_FORMAT}
+     */
+    @SerializedName("startTime")
+    private String startTime;
+
+    /**
+     * The end time of the meeting in format
+     * {@link com.example.companymeetingscheduler.Helper.TimeAndDateUtils#DEFAULT_DATE_FORMAT}
+     */
+    @SerializedName("endTime")
+    private String endTime;
+
+    /**
+     * The description about the meeting
+     */
+    @SerializedName("description")
+    private String description;
+
+    /**
+     * The names of the participants that will be taking part in the meeting
+     */
+    @SerializedName("participants")
+    private String[] participants;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String[] getParticipants() {
+        return participants;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setParticipants(String[] participants) {
+        this.participants = participants;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+}
